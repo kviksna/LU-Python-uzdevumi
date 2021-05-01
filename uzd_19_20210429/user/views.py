2a750d5b2d41618df0f2757657a2fa6e3b096fd3
kviksna <kviksna@inbox.lv>
2021-05-01 22:30:55 +0300

    Uzdevums 19, 2021.04.29 Added CBV for user-list, user-detail

---
+++ uzd_19_20210429/user/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import render, HttpResponse
+from user.models import User
+from django.views.generic import View, ListView, FormView, DetailView
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'index_list.html'
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_detail.html'
+
+
+def index(request):
+
+    users = User.objects.all()
+
+    context = {
+        'users': users,
+    }
+
+    return render(
+        template_name='index.html',
+        request=request,
+        context=context,
+    )
+
+
+def add_user(request):
+
+    if request.method == 'POST':
+
+        user = User(
+            username=request.POST['name'],
+            email=request.POST['email'],
+        )
+
+        user.save()
+
+        context = {
+            'user': user,
+        }
+
+        return render(
+            template_name='user.html',
+            request=request,
+            context=context,
+
+        )
+
+    return render(
+        template_name='form.html',
+        request=request
+    )
+
+
+def get_user(request, user_id):
+
+    user = User.objects.get(pk=user_id)
+
+    context = {
+        'user': user,
+    }
+
+    return render(
+        template_name='user.html',
+        request=request,
+        context=context,
+
+    )
+
+
+def delete_user(request, user_id):
+
+    user = User.objects.get(pk=user_id)
+    user.delete()
+
+    return HttpResponse(f'Deleted {user.username}')
+
+
+def edit_user(request, user_id):
+
+    user = User.objects.get(id=user_id)
+
+    if request.method == 'POST':
+
+        username = request.POST['name']
+        email = request.POST['email']
+
+        if len(username) != 0:
+            user.username = username
+
+        if len(email) != 0:
+            user.email = email
+
+        user.save()
+
+        context = {
+            'user': user,
+        }
+
+        return render(
+            template_name='user.html',
+            request=request,
+            context=context,
+
+        )
+
+    return render(
+        template_name='form.html',
+        request=request
+    )
