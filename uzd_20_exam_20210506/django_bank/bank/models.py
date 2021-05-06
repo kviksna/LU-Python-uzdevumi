8dc4d3e3e3350053b1302ac4365aef61a64a9f43
kviksna <kviksna@inbox.lv>
2021-05-06 21:14:13 +0300

    Last try

--- uzd_20_exam_20210506/django_bank/bank/models.py
+++ uzd_20_exam_20210506/django_bank/bank/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 
 class Deposit(models.Model):
+
     def __init__(self):
         interest = 0
         for i in range(self.term):
             interest += self.deposit * self.rate
         self.interest = interest
 
-    deposit = models.IntegerField()
-    term = models.IntegerField()
-    rate = models.FloatField()
+    deposit = models.IntegerField(verbose_name="Deposit (euro)")
+    term = models.IntegerField(verbose_name="Term (years)")
+    rate = models.FloatField(verbose_name="Rate (decimal")
+
+    @property
+    def interest(self):
+        interest = 0
+        for i in range(self.term):
+            interest += self.deposit * self.rate
+        self.interest = interest
