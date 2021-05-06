00ec4d473376b0404970584c563fdf81199ee087
kviksna <kviksna@inbox.lv>
2021-05-06 20:43:54 +0300

    Exam, interest added -project stopped running, see running version before this commit! Be mercy!

--- uzd_20_exam_20210506/django_bank/bank/models.py
+++ uzd_20_exam_20210506/django_bank/bank/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Deposit(models.Model):
+    def __init__(self):
+        interest = 0
+        for i in range(self.term):
+            interest += self.deposit * self.rate
+        self.interest = interest
 
     deposit = models.IntegerField()
     term = models.IntegerField()
